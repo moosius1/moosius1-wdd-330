00b6300c306addc2e02ad4afa6471cc009848ae2
moosius1 <imoe61@gmail.com>
2021-12-11 10:26:17 -0700

    updates

--- week10/week10.js
+++ week10/week10.js
@@ -1,20 +1,3 @@
-  fetch("https://jokeapi-v2.p.rapidapi.com/joke/Any?format=json&contains=C%2523&idRange=0-150&blacklistFlags=nsfw%2Cracist", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "jokeapi-v2.p.rapidapi.com",
-		"x-rapidapi-key": "198d2ae886mshda0c71e675676f0p1e0877jsn724c64a80630"
-	}
-})
-.then((response => {
-    return response.json();
-	
-}))
-.catch(err => {
-	console.error(err);
-});
-
-
-
 
 
     var animateButton = function(e) {
